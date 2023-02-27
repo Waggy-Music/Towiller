be77f6ea88a74eacaaa36d7f44587e7df325c90e
Yusuke Kashiwagi <114566645+Waggy-Music@users.noreply.github.com>
2023-02-27 13:53:13 +0000

    deviseViewAdd

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
+  get 'users/show' => 'users#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
