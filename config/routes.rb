af927d5d1d2785a8d6ea0d7ba0a2cbcc851dc26f
Yusuke Kashiwagi <114566645+Waggy-Music@users.noreply.github.com>
2023-03-07 09:23:20 +0000

    PostMigrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  # get 'posts/new'
+  # get 'posts/show'
+  # get 'posts/index'
   root to: 'homes#top'
   devise_for :users
   get 'users/show' => 'users#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :posts
+  
 end
