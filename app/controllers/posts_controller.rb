15ab983294f1df25d934819cb22ae85d90ba4bc9
Yusuke Kashiwagi <114566645+Waggy-Music@users.noreply.github.com>
2023-03-09 14:55:59 +0000

    postNewfix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,27 @@
 class PostsController < ApplicationController
   def new
+    @post=Post.new
   end
   
   def create
-    @post=Post.new(post_params)
+    @post=Post.new(post_param)#単数
     @post.save
-    redirect_to post_path(@post.id)
+    redirect_to posts_path
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def index
+    @posts = Post.all
+    # @user = @post.user
   end
+  
+  private
+  
+  def post_param
+    params.require(:post).permit(:body)
+  end
+  
 end
