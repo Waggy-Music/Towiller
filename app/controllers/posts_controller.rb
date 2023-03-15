8c2a18ab36fb5160ecae410301e1d6e5ef816ab7
Yusuke Kashiwagi <114566645+Waggy-Music@users.noreply.github.com>
2023-03-15 14:03:03 +0000

    associationFix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,7 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     # binding.pry
-    @post.user_id = current_user.id
+    # @post.user_id = current_user.id
     @post.save
     redirect_to posts_path
   end
